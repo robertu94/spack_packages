ab9e9b15342b0463209b065c4fa0b1a60af6ff9a
Robert Underwood <rr.underwood94@gmail.com>
2020-12-03 14:31:24 -0500

    libpressio bugfix release

--- packages/libpressio/package.py
+++ packages/libpressio/package.py
@@ -10,6 +10,7 @@ class Libpressio(CMakePackage):
 
     version('master', branch='master')
     version('develop', branch='develop')
+    version('0.53.1', sha256='1425dec7ee1a7ddf1c3086b83834ef6e49de021901a62d5bff0f2ca0c75d3455')
     version('0.53.0', sha256='0afb44c2dab8dd8121d174193eb6623b29da9592e5fe1bbe344cfc9cacbec0cb')
     version('0.52.2', sha256='c642463da0bbdd533399e43c84ea0007b1d7da98276c26bc075c7b4778f97a01')
     version('0.52.1', sha256='32f211aaf4641223bf837dc71ea064931f85aa9260b9c7f379787ca907c78c3a')
