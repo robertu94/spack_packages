76a87bf164b9276ff2cc78a47d0e7fb4966f09b3
Robert Underwood <rr.underwood94@gmail.com>
2021-07-16 19:28:16 -0400

    unstable z-checker-tools package

---
+++ packages/z-checker-tools/package.py
@@ -0,0 +1,51 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install zchecker-tools
+#
+# You can edit this file again by typing:
+#
+#     spack edit zchecker-tools
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class ZCheckerTools(CMakePackage):
+    """A collection of analysis tools that build upon z-checker"""
+
+    homepage = "https://github.com/CODARcode/z-checker-installer"
+    url      = "https://github.com/CODARcode/z-checker-installer/archive/refs/tags/0.7.0.tar.gz"
+    git      = "https://github.com/CODARcode/z-checker-installer"
+
+    maintainers = ['disheng222', 'robertu94']
+
+    version('develop', branch='develop')
+    version('develop-robertu94', branch='develop', git="https://github.com/robertu94/z-checker-installer")
+
+    depends_on('libpressio@0.66.3+sz+zfp+fpzip+json')
+    depends_on('z-checker')
+    depends_on('perl')
+    depends_on('git')
+    depends_on('gnuplot@5.0.6:')
+    depends_on('libpng@1.6.37:')
+    depends_on('tif22pnm')
+    depends_on('sam2p')
+    depends_on('texlive')
+    depends_on('ghostscript')
+
+    def cmake_args(self):
+        args = []
+        return args
