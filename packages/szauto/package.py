aff2692e8dbe933f62290936bffa15ec01f89693
Robert Underwood <rr.underwood94@gmail.com>
2021-07-29 08:20:04 -0400

    fix szauto

--- packages/szauto/package.py
+++ packages/szauto/package.py
@@ -33,6 +33,7 @@ class Szauto(CMakePackage):
     maintainers = ['disheng222', 'robertu94']
 
     version('master', branch='master')
+    version('1.2.1', sha256='55c58f58df3a874f684ef864a9247907df0501e5598c089fd2d855ae0309b03a')
     version('1.0.0', commit="03f3ab0312bd1de647e9d65746add73a0e8602d2")
 
     depends_on('zstd')
