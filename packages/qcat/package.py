b3b6eba1ef72c1b1e75bc87f0ef8de6a39dcbf04
Robert Underwood <rr.underwood94@gmail.com>
2022-01-06 14:30:57 -0500

    fix qcat building

--- packages/qcat/package.py
+++ packages/qcat/package.py
@@ -23,24 +23,22 @@
 from spack import *
 
 
-class Qcat(AutotoolsPackage):
+class Qcat(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/szcompressor/qcat"
-    url      = "https://github.com/szcompressor/qcat/archive/refs/tags/1.2.tar.gz"
-    git =      "https://github.com/szcompressor/qcat"
+    git =      "https://github.com/robertu94/qcat"
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
+    maintainers = ['disheng222', 'robertu94']
 
     version('master', branch="master")
-    version('1.2', sha256='83197c1ab902558a85ab5d33d1e25d93536d47b21eefc5060cce3f674c00492e')
 
     # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    depends_on('zstd')
 
-    def configure_args(self):
-        args = []
+    def cmake_args(self):
+        args = ["-DQCAT_USE_BUNDLES=OFF"]
         return args
