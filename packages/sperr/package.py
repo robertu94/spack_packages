2fac0cfe9d05a36a5b80e0286e5c724e359d0b57
Robert Underwood <runderwood@anl.gov>
2022-05-26 19:51:17 -0400

    add libpressio-sperr

--- packages/sperr/package.py
+++ packages/sperr/package.py
@@ -27,23 +27,26 @@ class Sperr(CMakePackage):
     """SPERR is a lossy scientific (floating-point) data compressor that can perform either error-bounded or size-bounded data compression"""
 
     homepage = "https://github.com/shaomeng/SPERR"
-    git      = "https://github.com/shaomeng/SPERR"
+    git      = "https://github.com/robertu94/SPERR"
 
-    version('2022.05.25', commit="6e73e57fe4343f9e8bb4ed42cf677b6cba99f570")
+    version('2022.05.26', commit="7894a5fe1b5ca5a4aaa952d1779dfc31fd741243")
 
-    depends_on('git')
+    depends_on('git', type='build')
+    depends_on('zstd', type=('build','link'), when="+zstd")
+    depends_on('pkgconfig', type=('build'), when="+zstd")
 
     variant('shared', description="build shared libaries", default=True)
     variant('qz', description="coding terminates by quantization level", default=True)
-    variant('zstd', description="use Zstd for more compression", default=False)
+    variant('zstd', description="use Zstd for more compression", default=True)
 
     def cmake_args(self):
         args = [
             self.define_from_variant('BUILD_SHARED_LIBS',  'shared'),
             self.define_from_variant('QZ_TERM',  'qz'),
-            "-DUSE_ZSTD=OFF",
-            #self.define_from_variant('USE_ZSTD',  'zstd'), TODO remove packaged deps
-            "-DBUILD_CLI_UTILITIES=OFF"
-            "-DBUILD_UNIT_TESTS=OFF"
+            self.define_from_variant('USE_ZSTD',  'zstd'),
+            "-DSPERR_PREFER_RPATH=OFF",
+            "-DUSE_BUNDLED_ZSTD=OFF",
+            "-DBUILD_CLI_UTILITIES=OFF",
+            "-DBUILD_UNIT_TESTS=OFF",
         ]
         return args
