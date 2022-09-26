f7f583789b1a3f64dba6a3edafa4bc3acd493654
Robert Underwood <runderwood@anl.gov>
2022-09-26 16:27:56 -0400

    fix develop for libpressio opt

--- packages/libpressio-opt/package.py
+++ packages/libpressio-opt/package.py
@@ -5,7 +5,7 @@ class LibpressioOpt(CMakePackage):
     """Metacompressor which preforms optimization of compressor settings for LibPressio"""
 
     homepage = "https://github.com/robertu94/libpressio_opt"
-    git      = "git@github.com:robertu94/libpressio_opt"
+    git      = homepage
     url      = "https://github.com/robertu94/libpressio_opt/archive/refs/tags/0.11.0.tar.gz"
 
     version('develop', branch='develop')
