e755caed2b47bc5d733d1d10a9e8ddf0c2c38b34
Robert Underwood <runderwood@anl.gov>
2022-10-26 13:19:33 -0400

    libpressio 0.88.1

--- packages/libpressio/package.py
+++ packages/libpressio/package.py
@@ -10,8 +10,11 @@ from spack.pkg.builtin.libpressio import Libpressio as BuiltinLibPressio
 class Libpressio(BuiltinLibPressio):
     """A generic abstraction for the compression of dense tensors"""
 
+    version("0.88.1", sha256="d7fe73a6b2d8de6d19c85e87888dcf1a62956f56b4e6dfd23e26901740031e00")
+
     variant("szx", default=False, description="build support for SZx", when="@0.87.0:")
     depends_on("szx", when="+szx")
+    
 
     def cmake_args(self):
         args = super().cmake_args()
