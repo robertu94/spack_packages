4369f8018cc9d9770521a32edcffbd704a4e5f8c
Robert Underwood <runderwood@anl.gov>
2023-06-14 14:57:22 -0400

    update libpressio-tools

--- packages/libpressio-tools/package.py
+++ packages/libpressio-tools/package.py
@@ -5,6 +5,8 @@ from spack.pkg.builtin.libpressio_tools import LibpressioTools as BuiltinLibPres
 class LibpressioTools(BuiltinLibPressioTools):
     """Pre-Release: General Utilities for LibPressio"""
 
+
+    version("0.4.1", sha256="c342866ce95167ff708c7b3559c43ee174b7f4e760940f1c145a2fb4b9250c83")
     version("0.4.0", sha256="c68ae3b0e73c7c4fb1562f3515d41a227eb4c0e842eb3106b719afcc1e35a7d9")
 
     depends_on('libpressio-frsz', when="+frsz")
