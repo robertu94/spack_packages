f6729a049bd08674005070d89cbbd76f08d4cc68
Robert Underwood <runderwood@anl.gov>
2023-08-29 19:29:20 -0400

    update sz to have the openmp variant

---
+++ packages/sz/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+from spack.pkg.builtin.sz import Sz as SzBuiltin, CMakeBuilder as SZCMakeBuilder
+
+
+class Sz(SzBuiltin):
+    variant("openmp", default=False, description="build the multithreaded version using openmp")
+
+
+class CMakeBuilder(SZCMakeBuilder):
+    def cmake_args(self):
+        args = super().cmake_args()
+        args.append(self.define_from_variant("BUILD_OPENMP", "openmp"))
+        print(args)
+        return args
