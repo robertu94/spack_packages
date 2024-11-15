6d484c4776b6d246980baa4b5ac3d5cc1b40c054
Hrithik Devaiah Bollachettira Ajithkumar <142629615+hrithikdevaiah-999@users.noreply.github.com>
2024-11-15 09:54:12 -0500

    Update package.py

--- packages/fzvis/package.py
+++ packages/fzvis/package.py
@@ -11,12 +11,14 @@ class Fzvis(Package):
 
     homepage = "https://szcompressor.org/next.szcompressor.github.io/"
     url = "https://github.com/YuxiaoLi1234/fzvis/archive/refs/tags/v0.2.2.tar.gz"
+    git = "https://github.com/YuxiaoLi1234/fzvis"
 
     maintainers("robertu94", "YuxiaoLi1234", "hrithikdevaiah-999")
 
     license("MIT")
 
-    version("0.2.0", sha256="bd61d0210adcb70b6b2d189905b6585cd7cf3ea9840a59e6fdae14ea5bb3487d", url="https://github.com/YuxiaoLi1234/fzvis/archive/refs/tags/v0.2.0.tar.gz")
+    version("0.2.0", sha256="bd61d0210adcb70b6b2d189905b6585cd7cf3ea9840a59e6fdae14ea5bb3487d", 
+	url="https://github.com/YuxiaoLi1234/fzvis/archive/refs/tags/v0.2.0.tar.gz")
    
     version("0.2.1", sha256="7a42bf5d40c1e331d43a0ee1482f6104f0d5d4254e9d2d469362dcde4a7e8fc8", 
 	url="https://github.com/YuxiaoLi1234/fzvis/archive/refs/tags/v0.2.1.tar.gz")
