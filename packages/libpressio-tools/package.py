bee438626e101bc2c34132478d52e9a8b244a7ad
Robert Underwood <runderwood@anl.gov>
2024-11-17 17:51:25 -0500

    update fix default compressor

--- packages/libpressio-tools/package.py
+++ packages/libpressio-tools/package.py
@@ -11,6 +11,7 @@ class LibpressioTools(BuiltinLibPressioTools):
     variant("frsz", default=False, description="depend on frsz", when="@0.1.2:")
     variant("lcgpu", default=False, description="depend on lc-gpu", when="@0.5.0:")
 
+    version("0.5.1", sha256="0146e26dc7fb3bae05edb9d4143cea39018c873a4e53be017271c1e6a00d696d")
     version("0.5.0", sha256="6fbab7fc2ce6dc65f9fa84c6e07feee22ae67d133b46a42e91c775cc99d6071c")
     depends_on("lc-framework@1.3.0:+libpressio+cuda", when="+lcgpu")
     depends_on("libpressio@1.0.0:", when="@0.5.0:")
