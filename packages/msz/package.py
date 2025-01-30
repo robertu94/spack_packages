dd2e658c7ed41ce8f36ddae445179c0d260f7e89
Yuxiao Li <yli14@ascend-rw02.ten.osc.edu>
2025-01-30 12:19:51 -0500

    updated version

--- packages/msz/package.py
+++ packages/msz/package.py
@@ -27,13 +27,18 @@ class Msz(CMakePackage, CudaPackage):
     """An Efficient Parallel Algorithm for Correcting Morse-Smale Segmentations in Error-Bounded Lossy Compressors"""
 
     homepage = "https://github.com/YuxiaoLi1234/MSz"
+    url = "https://github.com/YuxiaoLi1234/MSz/archive/refs/tags/v0.0.2.tar.gz"
     git = "https://github.com/YuxiaoLi1234/MSz"
 
     maintainers("YuxiaoLi1234", "robertu94")
 
     license("MIT", checked_by="robertu94")
 
-    version("dev", branch="main")
+    version("main", branch="main")
+    version("0.0.2", sha256="7f88bf29b6ab7035246173c59b5fa6e3c6b31cab53e5b241530ae7bf72befa13")
+    version("0.0.1", sha256="504bed79593ec7605a4a3c63cb038163e9de6fea8f8d75487001612e83bd9073")
+    
+
 
     variant("zstd", default=True, description="Enable Zstd compression")
     variant("cuda", default=False, description="Enable CUDA support")
